40d2540588eb3261dddb02c6748120d9baa53c36
Yue Li <liyuesa0616@gmail.com>
2024-06-25 21:34:00 +0800

    update reference_count
    add author list

--- reference_count.py
+++ reference_count.py
@@ -3,7 +3,7 @@ import glob
 import pyrebase
 
 TI = set()
-cited_count = dict()
+reference_count = dict()
 
 file_paths = glob.glob(os.path.join("autonomous_vehicle_data_records", "*.txt"))
 
@@ -13,40 +13,46 @@ for file_path in file_paths:
     with open(file_path, 'r', encoding="utf-8") as file:
         # initialize keyword and insideDE
         ti = ""
-        pos = ""
+        author = []
         cnt = 0
         insideTI = False
+        insideAF = False
         # iterate through each line
         for line in file:
+            if line.startswith("AF "):
+                insideAF = True
+                author.append(line[3:].strip())
+            elif line.startswith("   ") and insideAF:
+                author.append(line[3:].strip())
+            else :
+                insideAF = False
+            # starts of a title
             if line.startswith("TI "):
                 insideTI = True
                 ti += line[3:].strip()
-
             elif line.startswith("   ") and insideTI:
                 ti += line[3:].strip()
             elif line.startswith("SO "):
-                pos = file_path.split('\\')[-1]
                 insideTI = False
             elif line.startswith("Z9 "):
                 cnt = int(line[3:].strip())
-                TI.add(ti)
                 if ti != "":
-                    if cited_count.get(ti, False):
-                        cited_count[ti]["cited"] += cnt
-                        cited_count[ti]["pos"] += f",{pos}"
+                    if reference_count.get(ti, False):
+                        reference_count[ti]["reference"] += cnt
                     else:
-                        cited_count[ti] = dict()
-                        cited_count[ti]["cited"] = cnt
-                        cited_count[ti]["pos"] = pos
+                        TI.add(ti)
+                        reference_count[ti] = dict()
+                        reference_count[ti]["reference"] = cnt
+                        reference_count[ti]["author"] = author
 
                 ti = ""
-                pos = ""
+                author = []
                 cnt = 0
                 insideTI = False
 
 # sort the dictionary with it's value in descending order
-sorted_TI = sorted(cited_count.items(), key=lambda x: x[1]["cited"], reverse=True)
+sorted_reference = sorted(reference_count.items(), key=lambda x: x[1]["reference"], reverse=True)
 
-with open('output_cited.txt', 'w') as file:
-    for key, value in sorted_TI:
-        print(f"{key}(Cited count:{value.get('cited')})({value.get('pos')})", file=file)
+with open('output_reference.txt', 'w') as file:
+    for key, value in sorted_reference:
+        print(f"{key}(Reference count:{value.get('reference')})({value.get('author')})", file=file)
